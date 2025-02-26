4232da62ec915b7f3cd773845f83754e11652ff1
chloehkim <chloekim6156@gmail.com>
2025-02-26 01:02:55 +0000

    new

---
+++ lab0.c
@@ -0,0 +1,29 @@
+/***************************************************************** 
+
+    File: lab0.c
+    Author: [Your Name]
+    Student Number: [Your Student Number]
+
+    To compile program in codespaces, in terminal pane type:
+        gcc -Wall lab0.c
+    To run program in codespaces, in terminal pane type:
+        ./a.out
+        
+***************************************************************/
+
+#include <stdio.h>  /*copy and paste command to put contents of
+                        stdio.h to where this statement is*/
+
+/*all programs start with the main function.
+this function accepts nothing and returns an integer indicating if
+there were any error*/
+int main(void)
+{
+    /*call the printf() function with "hello world\n"*/
+    printf("hello world!!\n");
+    printf("My name is Chloe Kim.\n");
+    printf("I'm from Korea and here to learn about Computer Programming which is new world for me.\n");
+    printf("I'm a big fan of League of Legend, but unfortunately don't play the game. just love watching the games.\n");
+    printf("If anyone enjoys watching League of legend, let's watch the games together!\n");
+    return 0;
+}
